c80c3efeeaaeef423404452ccf5948e428363776
Siddhant Gupta <71279017+siddhantgupta547@users.noreply.github.com>
2023-07-23 18:34:55 +0530

    Time: 75 ms (28.80%), Space: 43.3 MB (80.83%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -8,11 +8,14 @@ var majorityElement = function(nums) {
         if(nums[i] in obj)  obj[nums[i]]+=1;
         else    obj[nums[i]]=1;
     }
-    const count=Object.values(obj);
-    const values=Object.keys(obj)
-    count.forEach((Item,index)=>{
-        if(Item>nums.length/2) value= values[index];
-    })
+    // const count=Object.values(obj);
+    // const values=Object.keys(obj)
+    // count.forEach((Item,index)=>{
+    //     if(Item>nums.length/2) value= values[index];
+    // })
+    for(let key in obj){
+        if(obj[key]>nums.length/2)  value=key;
+    }
     
     return value;
 };
