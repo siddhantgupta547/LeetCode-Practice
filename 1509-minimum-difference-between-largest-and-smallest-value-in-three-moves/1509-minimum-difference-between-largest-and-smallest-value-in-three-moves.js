27a4dff0e70e0c8bb3a69ab65257220109e392a5
Siddhant Gupta <71279017+siddhantgupta547@users.noreply.github.com>
2024-07-08 09:30:11 +0530

    Time: 110 ms (85.40%), Space: 58.1 MB (67.21%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.js
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.js
@@ -8,7 +8,6 @@ var minDifference = function(nums) {
         return 0;
     }
     const sortedNums= nums.sort((a,b)=>a-b)
-    console.debug(sortedNums)
     let minimum=sortedNums[n-4]-sortedNums[0];
     if(minimum>sortedNums[n-1]-sortedNums[3]){
         minimum=sortedNums[n-1]-sortedNums[3]
