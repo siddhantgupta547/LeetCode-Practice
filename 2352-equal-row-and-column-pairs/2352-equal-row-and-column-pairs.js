5f1a8df41e509e0e8f50755e9767f3690ae31425
Siddhant Gupta <71279017+siddhantgupta547@users.noreply.github.com>
2024-07-29 02:24:30 +0530

    Time: 114 ms (45.29%), Space: 59.1 MB (51.48%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.js
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.js
@@ -4,18 +4,42 @@
  */
 var equalPairs = function(grid) {
     let result=0
+    //brute force
+    // for(let row=0;row<grid[0].length;row++){
+    //     for(let col=0;col<grid[0].length;col++){
+    //         let isEqual=true
+    //         for(let elem=0;elem<grid[0].length;elem++){
+    //             if(grid[elem][col]!==grid[row][elem]){
+    //                 isEqual=false
+    //                 break;
+    //             }
+    //         }
+    //         if(isEqual){
+    //             result++;
+    //         }
+    //     }
+    // }
+    
+    //HashMap
+    const map= new Map()
     for(let row=0;row<grid[0].length;row++){
+        let toBeStored=''
         for(let col=0;col<grid[0].length;col++){
-            let isEqual=true
-            for(let elem=0;elem<grid[0].length;elem++){
-                if(grid[elem][col]!==grid[row][elem]){
-                    isEqual=false
-                    break;
-                }
-            }
-            if(isEqual){
-                result++;
-            }
+            toBeStored+=grid[row][col]+','
+        }
+        if(map.has(toBeStored)){
+            map.set(toBeStored, (map.get(toBeStored)+1))
+        }else{
+            map.set(toBeStored,1)
+        }
+    }
+    for(let col=0;col<grid[0].length;col++){
+        let toBeStored=''
+        for(let row=0;row<grid[0].length;row++){
+            toBeStored+=grid[row][col]+','
+        }
+        if(map.has(toBeStored)){
+            result+=map.get(toBeStored)
         }
     }
     return result
