fe4d754167938830c519457c667efcc6e5dec2d5
Siddhant Gupta <71279017+siddhantgupta547@users.noreply.github.com>
2024-08-02 02:16:39 +0530

    Time: 43 ms (97.20%), Space: 51 MB (45.22%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.js
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.js
@@ -4,7 +4,6 @@
  */
 var countSeniors = function(details) {
     let count=0;
-    let characterArray=[]
     details.forEach(item=>{
         const age = Number(item[11] + item[12])
         if(age>60)  count++
