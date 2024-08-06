23e12b79db7c0bf904aa4c914151a52abfaf4c38
Siddhant Gupta <71279017+siddhantgupta547@users.noreply.github.com>
2024-08-06 13:06:08 +0530

    Time: 173 ms (64.00%), Space: 57.9 MB (92.00%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var minimumPushes = function(word) {
+    const map= new Map()
+    const freqArray=[]
+    for(let i=0;i<word.length;i++){
+        if(map.has(word[i])){
+            map.set(word[i],map.get(word[i])+1)
+        }
+        else{
+            map.set(word[i],1)
+        }
+    }
+    map.forEach((value,key)=>{
+        freqArray.push([key,value])
+    })
+    freqArray.sort((a,b)=>b[1]-a[1])
+    map.delete()
+    let count=1;
+    let output=0
+    let numberToBeMappedTo=2
+    for(let i=0;i<freqArray.length;i++){
+        if(numberToBeMappedTo===10){
+            count++
+            numberToBeMappedTo=2;
+        }
+        output+=freqArray[i][1]*count
+        numberToBeMappedTo++
+    }
+    return output
+};
