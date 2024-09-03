2c13e5413990e9a37b3986393576304b39ef149a
Siddhant Gupta <71279017+siddhantgupta547@users.noreply.github.com>
2024-09-04 01:06:01 +0530

    Time: 56 ms (64.15%), Space: 49.2 MB (94.97%) - LeetHub

--- 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.js
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.js
@@ -5,11 +5,9 @@
  */
 var getLucky = function(s, k) {
     let characterToNumber=''
-    
     for(i=0;i<s.length;i++){
         characterToNumber+=(s.charCodeAt(i)-96)
     }
-    let transformK=Number(characterToNumber)
     while(k>0 && characterToNumber.length>1){
         let newNumber=0
     for(let i=0;i<characterToNumber.length;i++){
