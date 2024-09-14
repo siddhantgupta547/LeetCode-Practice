fda727546e3284d643e88f1d2989ab5c47a2a509
Siddhant Gupta <71279017+siddhantgupta547@users.noreply.github.com>
2024-09-15 01:39:36 +0530

    Time: 173 ms (37.50%), Space: 62.1 MB (40.18%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.js
@@ -0,0 +1,44 @@
+
+var MyHashMap = function() {
+    this.map={}
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+MyHashMap.prototype.put = function(key, value) {
+    this.map[key]=value
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+MyHashMap.prototype.get = function(key) {
+    if(key in this.map){
+        return this.map[key]
+    }else{
+        return -1
+    }
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashMap.prototype.remove = function(key) {
+    if(key in this.map){
+        delete this.map[key]
+    }
+    
+};
+
+/** 
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
