7ee00e00fd433ba72330c1d16a6e32d619120ab5
Siddhant Gupta <71279017+siddhantgupta547@users.noreply.github.com>
2024-09-18 00:22:24 +0530

    Time: 53 ms (58.61%), Space: 49.2 MB (71.72%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.js
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.js
@@ -4,30 +4,24 @@
  * @return {string[]}
  */
 var uncommonFromSentences = function(s1, s2) {
-    const s1Map= new Map()
-    const s2Map=new Map()
+    const map= new Map()
     s1.split(' ').forEach(item=>{
-        if(s1Map.has(item)){
-            s1Map.set(item,s1Map.get(item)+1)
+        if(map.has(item)){
+            map.set(item,map.get(item)+1)
         }else{
-            s1Map.set(item,1)
+            map.set(item,1)
         }
     })
     s2.split(' ').forEach(item=>{
-        if(s2Map.has(item)){
-            s2Map.set(item,s2Map.get(item)+1)
+        if(map.has(item)){
+            map.set(item,map.get(item)+1)
         }else{
-            s2Map.set(item,1)
+            map.set(item,1)
         }
     })
     const result=[]
-    s1Map.forEach((value,key)=>{
-        if(value<2 && !s2Map.has(key)){
-            result.push(key)
-        }
-    })
-    s2Map.forEach((value,key)=>{
-        if(value<2 && !s1Map.has(key)){
+    map.forEach((value,key)=>{
+        if(value<2){
             result.push(key)
         }
     })
