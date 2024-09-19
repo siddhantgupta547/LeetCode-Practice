e4e33e0dd977910776735c82391b2e993a461ba5
Siddhant Gupta <71279017+siddhantgupta547@users.noreply.github.com>
2024-09-19 23:59:52 +0530

    Time: 67 ms (16.13%), Space: 50.6 MB (73.66%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var largestNumber = function(nums) {
+    const sortedString = nums.sort((a,b)=>{
+        const stringA=`${a}`
+        const stringB=`${b}`
+        if(stringA+stringB>stringB+stringA){
+            return -1
+        }else if(stringA+stringB<stringB+stringA){
+            return 1
+        }else{
+            return 1
+        }
+    }).join('')
+    if(Number(sortedString)===0){
+        return '0'
+    }
+    return sortedString
+};
