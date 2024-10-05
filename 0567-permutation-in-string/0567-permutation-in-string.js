8761c91e1742462950eb373267c86e2479e0a70a
Siddhant Gupta <71279017+siddhantgupta547@users.noreply.github.com>
2024-10-05 21:55:12 +0530

    Time: 2945 ms (5.03%), Space: 56.9 MB (24.42%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -4,6 +4,9 @@
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
+    if(s1.length>s2.length){
+        return false
+    }
     const obj=new Object()
     for(let i=0;i<s1.length;i++){
         if(s1[i] in obj){
