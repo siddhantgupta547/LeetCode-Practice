b9fd3aa1f2d95244bc06d15159c2374d1d2aac8c
Siddhant Gupta <71279017+siddhantgupta547@users.noreply.github.com>
2024-11-01 13:22:28 +0530

    Time: 70 ms (16.38%), Space: 54.9 MB (14.60%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -25,10 +25,23 @@ function currentIndex(index,lastIndex,nums,set){
 }
 
 var canJump = function(nums) {
-    const set=new Set()
-   if(currentIndex(0,nums.length-1,nums,set)){
-        return true
-   }else{
-    return false
-   }
+   //  const set=new Set()
+   // if(currentIndex(0,nums.length-1,nums,set)){
+   //      return true
+   // }else{
+   //  return false
+   // }
+    let bottomUp=new Array(nums.length)
+    bottomUp[0]=true
+    for(let i=1;i<nums.length;i++){
+        for(let j=i-1;j>=0;j--){
+            if(bottomUp[j] && j+nums[j]>=i){
+                bottomUp[i]=true
+                break
+            }else{
+                bottomUp[i]=false
+            }
+         }
+    }
+    return bottomUp[nums.length-1]
 };
