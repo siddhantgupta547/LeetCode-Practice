4f39936cdc66c0d31b455435271982193b90f5b4
Siddhant Gupta <71279017+siddhantgupta547@users.noreply.github.com>
2024-11-03 12:26:53 +0530

    Time: 0 ms (100.00%), Space: 48.6 MB (86.87%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.js
+++ 0796-rotate-string/0796-rotate-string.js
@@ -6,18 +6,20 @@
 var rotateString = function(s, goal) {
     if(s.length!==goal.length)
         return false
-    if(s===goal){
-        return true
-    }
-    let count=0
-    let sArray=s.split('')
-    while(count<=s.length){
-        const first=sArray.shift()
-        sArray.push(first)
-        if(sArray.join('')===goal){
-            return true
-        }
-        count++
-    }
-    return false
+    // if(s===goal){
+    //     return true
+    // }
+    // let count=0
+    // let sArray=s.split('')
+    // while(count<=s.length){
+    //     const first=sArray.shift()
+    //     sArray.push(first)
+    //     if(sArray.join('')===goal){
+    //         return true
+    //     }
+    //     count++
+    // }
+    // return false
+    const con=s+s
+    return con.includes(goal)
 };
