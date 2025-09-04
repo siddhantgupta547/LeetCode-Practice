24e9c71c9392e1aeb1e493b1315f0ff5476b1eb7
Siddhant Gupta <71279017+siddhantgupta547@users.noreply.github.com>
2025-09-05 00:49:25 +0530

    0034-FIND-FIRST-AND-LAST-POSITION-OF-ELEMENT-IN-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 58.4 MB (5.51%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -6,15 +6,20 @@
 var searchRange = function(nums, target) {
     let first=-1
     let last=-1
+    let found=false
     function binarySearch(nums,target,left,right){
         if(right<left){
             return -1
         }
         const mid=left+Math.floor(right-left/2)
         let item=nums[mid]
+        if(found && item!==target){
+            return
+        }
         if(item===target){
             if(first-1 &&last ==-1){
                 first=last=mid
+                found=true
             }else if(mid<=first){
                 first=mid
             }else{
