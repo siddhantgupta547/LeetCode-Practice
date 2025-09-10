fdbefee93f1d6d2abee223031a272971ac6739ff
Siddhant Gupta <71279017+siddhantgupta547@users.noreply.github.com>
2025-09-11 01:18:50 +0530

    0540-SINGLE-ELEMENT-IN-A-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 58.2 MB (13.52%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
@@ -3,9 +3,27 @@
  * @return {number}
  */
 var singleNonDuplicate = function(nums) {
-    let xor=nums[0]
-    for(let idx=1;idx<nums.length;idx++){
-        xor=xor^nums[idx]
+    // let xor=nums[0]
+    // for(let idx=1;idx<nums.length;idx++){
+    //     xor=xor^nums[idx]
+    // }
+    // return xor
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let mid = left + Math.floor((right - left) / 2);
+
+        if (mid % 2 === 1) {
+            mid--;
+        }
+
+        if (nums[mid] === nums[mid + 1]) {
+            left = mid + 2;
+        } else {
+            right = mid;
+        }
     }
-    return xor
+
+    return nums[left]; 
 };
