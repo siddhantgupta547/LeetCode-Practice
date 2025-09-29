e96eb0337388bf66a13450e849509fa6b5dd4ed6
Siddhant Gupta <71279017+siddhantgupta547@users.noreply.github.com>
2025-09-29 20:18:00 +0530

    1646-KTH-MISSING-POSITIVE-NUMBER Stats: Time: 0 ms (100%), Space: 54.4 MB (39.57%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.js
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.js
@@ -3,20 +3,32 @@
  * @param {number} k
  * @return {number}
  */
-var findKthPositive = function(arr, k) {
-    let i=0
-    let j=0
-    let count=0
-    while(count!==k){
-        const arrItem=arr[i]
-        const expected=j+1
-        if(arrItem!==expected){
-            j++
-            count++
-        }else{
-            i++
-            j++
+var findKthPositive = function (arr, k) {
+    // let i=0
+    // let j=0
+    // let count=0
+    // while(count!==k){
+    //     const arrItem=arr[i]
+    //     const expected=j+1
+    //     if(arrItem!==expected){
+    //         j++
+    //         count++
+    //     }else{
+    //         i++
+    //         j++
+    //     }
+    // }
+    // return j
+    let left = 0
+    let right = arr.length - 1
+    while (left <= right) {
+        const mid = left + Math.floor((right - left) / 2)
+        const missingElements = arr[mid] - (mid + 1)
+        if (missingElements < k) {
+            left = mid + 1
+        } else {
+            right = mid - 1
         }
     }
-    return j
+    return left + k
 };
