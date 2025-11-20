87c7059b7376a3c43105e6c9902c450226a7b778
Siddhant Gupta <71279017+siddhantgupta547@users.noreply.github.com>
2025-11-20 23:13:42 +0530

    0104-MAXIMUM-DEPTH-OF-BINARY-TREE Stats: Time: 7 ms (5.1%), Space: 61.1 MB (5.2%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -13,7 +13,26 @@
 
 
 var maxDepth = function(root) {
-    if(!root) return 0;
+//    if(!root) return 0;
     
-    return Math.max(maxDepth(root.left) , maxDepth(root.right) ) +1
+//     return Math.max(maxDepth(root.left) , maxDepth(root.right) ) +1 
+
+//Iterative
+ if(!root){
+        return 0
+    }
+    const queue=[[1,root]]
+    let max=0
+    while(queue.length>0){
+        const [level,node]=queue.shift()
+        max=Math.max(max,level)
+        if(node.left){
+            queue.push([level+1,node.left])
+        }
+
+        if(node.right){
+            queue.push([level+1,node.right])
+        }
+    }
+    return max
 };
